37f60b36344db156e706391e9faa1545af2e9d96
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2024-02-26 11:37:19 -0300

    creating the pattern structure

--- airplane_factory/airplane.py
+++ airplane_factory/airplane.py
@@ -32,6 +32,7 @@ class comercial(ABC):
     def exibirInfoComercial(self):
         pass
 
+#Embraer
 class ejetse2(comercial):
     def exibirInfoComercial(self):
         print("\n")
@@ -43,6 +44,21 @@ class ejetse2(comercial):
         print("Tripulação: 2(Piloto e Co-Piloto)")
         print("Carga Útil: 15.150kg")
         print("Dimensões: 41m x 10,9m")
+        print("\n")
+#Boeing
+class boeing777(comercial):
+    def exibirInfoComercial(self):
+        print("\n")
+        print("--- Boeing 777 ---")
+        print("Avião Comercial")
+        print("Motor: Bimotor")
+        print("Fabricante: Embraer")
+        print("Passageiros: 314 a 550")
+        print("Tripulação: 2(Piloto e Co-Piloto)")
+        print("Carga Útil: 103.000kg")
+        print("Dimensões: 50m x 12m")
+        print("\n")
+
 
 
 
@@ -52,6 +68,7 @@ class military(ABC):
     def exibirInfoMilitary(self):
         pass
 
+#Embraer
 class c390(military):
     def exibirInfoComercial(self):
         print("\n")
@@ -63,6 +80,7 @@ class c390(military):
         print("Tripulação: 3(Piloto, Co-Piloto e Engenheiro de Voo)")
         print("Carga Útil: 26.000kg")
         print("Dimensões: 35,2m x 11,8m")
+        print("\n")
 
 
 
